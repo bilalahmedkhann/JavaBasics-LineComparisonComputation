db9ce97c68b44fea0f314c19ed1da5d7c19a76f1
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-18 20:13:01 +0530

    ADDED: Code to check if two lines are equal

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -1,23 +1,47 @@
 import java.util.Scanner;
 
-public class LineComparisonComputation {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+class LineComparisonComputation {
+    public static void main(String[] arg) {
+        // Declaration Part
         int x1;
         int x2;
         int y1;
         int y2;
-        double length_of_line;
+        int a1;
+        int a2;
+        int b1;
+        int b2;
+        double lengthOfLine1;
+        double lengthOfLine2;
+
+        // Get the input x1,y1,x2,y2,a1,a2,b1,b2 from the user
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter x1 point :");
         x1 = sc.nextInt();
-        System.out.println("Enter x2 point :");
-        x2 = sc.nextInt();
         System.out.println("Enter y1 point :");
         y1 = sc.nextInt();
+        System.out.println("Enter x2 point :");
+        x2 = sc.nextInt();
         System.out.println("Enter y2 point :");
         y2 = sc.nextInt();
-        length_of_line = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length between points "+"("+x1+","+y1+")&"+"("+x2+","+y2+")===>"+length_of_line);
+        System.out.println("Enter a1 point :");
+        a1 = sc.nextInt();
+        System.out.println("Enter b1 point :");
+        b1 = sc.nextInt();
+        System.out.println("Enter a2 point :");
+        a2 = sc.nextInt();
+        System.out.println("Enter b2 point :");
+        b2 = sc.nextInt();
+
+        lengthOfLine2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+        System.out.println("Length between " + "(" + a1 + "," + b1 + ")," + "(" + a2 + "," + b2 + ")===>" + lengthOfLine2);
+        lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length between " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + lengthOfLine1);
+
+        if (lengthOfLine1 == lengthOfLine2) {   // Check the Lines are equal or not
+            System.out.println("Both Lines are Equals");
+        } else
+            System.out.println("Both Lines are Not Equals");
     }
 }
